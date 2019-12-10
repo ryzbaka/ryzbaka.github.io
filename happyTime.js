74658fdc3e3c04a77d751129058b215686942523
Hamza Ali <hamzaali151214@hotmail.com>
2019-12-10 14:06:23 +0530

    Update happyTime.js

--- happyTime.js
+++ happyTime.js
@@ -1,9 +1,11 @@
 const backgroundDetails={freeze:false}
+let circleSize=50
 function setup() {
   createCanvas(400,400)
 }
 function mouseReleased(){
     backgroundDetails.freeze=true
+    circleSize=100
 }
 function draw() {
   const mappedX=map(mouseX,0,400,0,255)
@@ -14,7 +16,8 @@ function draw() {
   if(mouseIsPressed){
     fill(random(0,255),random(0,255),random(0,255),70)
     stroke('white')
-    ellipse(mouseX,mouseY,50,50)
+    ellipse(mouseX,mouseY,circleSize,circleSize)
+    circleSize-=1
   }
   if(!backgroundDetails.freeze){
   background(mappedX,mappedY,mappedX-mappedY+100)
