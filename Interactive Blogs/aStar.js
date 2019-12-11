0466022ae07633704e8cef235d96ed66a061c178
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-12-11 20:55:09 +0530

    made more additions to A*

--- Interactive Blogs/aStar.js
+++ Interactive Blogs/aStar.js
@@ -1,15 +1,31 @@
-let seletedBox
-let displayFlag=0
+let currentX=0
+let currentY=0
+let pathQueue=[]
+let startid
+let goalid
+let startX
+let startY
+let goalX
+let goalY
 class Node{
   constructor(i,j,identifier){
-    this.color='lightgrey'
+    this.color='lightblue'
     this.x=i
     this.y=j
     this.id=identifier
     this.processed=false
+    this.inPath=false
+    this.isGoal=false
+    this.isStart=false
   }
   changeColor(someColor){
     this.color=someColor
+  }
+  show(){
+    fill(this.color)
+    rect(this.x,this.y,50,50)
+    fill('white')
+    text(this.id,this.x,this.y)
   }
 }
 const nodes=[]
@@ -27,18 +43,59 @@ function setup() {
       fill('white')
       text(count,i,j)
       fill('lightblue')
-      count+=1
       const newNode=new Node(i,j,count)
+      count+=1
       colNodes.push(newNode)
     }
     nodes.push(colNodes)
   }
-  if(displayFlag===0){
-    console.log(nodes)
-    displayFlag=1
-  }
 }
-function restrictedDraw(){
-    console.log('lmao')
-    console.log(nodes)
+function processNode(someNode){
+    someNode.processed=true
+    updateColor(someNode)//updates the color value of a node
+    //based on weather it has been processed or has been added to path
+}
+function updateColor(someNode){
+    if(someNode.processed){
+        someNode.color="green"
+    }
+    else{
+        someNode.color="lightblue"
+    }
+    if(someNode.inPath){
+        someNode.color="lightgrey"
+    }
+    if(someNode.isGoal){
+        someNode.color="golden"
+    }
+}
+function drawNodes(){
+    /*fill(this.color)
+    rect(this.x,this.y,50,50)
+    fill('white')
+    text(this.id,this.x,this.y)*/
+    for(let i=0;i<10;i++){
+        for(let j=0;j<10;j++){
+            nodes[i][j].show()
+        }
+    }
+}
+function resetNodes(){
+    currentX=0
+    currentY=0
+    pathQueue=[]
+    for(let i=0;i<10;i++){
+        for(let j=0;j<10;j++){
+            nodes[i][j].processed=false
+            nodes[i][j].inPath=false
+            updateColor(nodes[i][j])
+        }
+    }
+    drawNodes()
+}
+function restrictedDraw() {
+    //write code here to start with start node
+    //add to path
+    //calculate f value of all neighbours
+    //move currentX and currentY to the index of the node with the minimum f value
 }
