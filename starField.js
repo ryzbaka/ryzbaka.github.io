ce4aaf12686ae6f914d8941d7a32aa9ac4f82878
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-12-11 05:38:20 +0530

    starfield

---
+++ starField.js
@@ -0,0 +1,52 @@
+let backCanvas;
+let frontCanvas;
+class Star{
+    constructor(){
+      this.x=random(-displayWidth,displayWidth);
+      this.y=random(-displayHeight,displayHeight);
+      this.z=random(displayWidth);
+    }
+    show(){
+      fill(255);
+      noStroke();
+      
+      const sx=map(this.x/this.z,0,1,0,displayWidth)
+      const sy=map(this.y/this.z,0,1,0,displayHeight)
+      const r=map(this.z,0,width,16,0)
+      ellipse(sx,sy,r,r)
+    }
+    update(){
+      this.z-=20
+      if(this.z<1){
+        this.z=displayWidth
+        this.x=random(-displayWidth,displayWidth);
+        this.y=random(-displayHeight,displayHeight);
+      }
+    }
+    
+  }
+  const nStars=1000
+  const stars=[]
+  function setup(){
+    backCanvas=createCanvas(displayWidth,displayHeight)
+    backCanvas.position(0,0)
+    backCanvas.style('z-index','-2')
+    backCanvas.background('black')
+    for(let i=0;i<nStars;i++){
+      const someStar=new Star()
+      stars.push(someStar)
+    }
+  }
+  function windowResized(){
+      backCanvas.height=displayHeight;
+      backCanvas.width=displayWidth;
+  }
+  function draw(){
+    background('black')
+    translate(displayWidth/2,displayHeight/2)
+    for(let i=0;i<nStars;i++){
+      stars[i].update()
+      stars[i].show()
+    }
+  }
+  
